ca885adb7f4efbb76c15f902287088c6eedddeb3
lostdawg3 <lostdawg3@yahoo.com>
2024-08-30 07:37:40 -0700

    Initial Commit

---
+++ SwiftSourceControlBootcamp/SwiftSourceControlBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlBootcampApp.swift
+//  SwiftSourceControlBootcamp
+//
+//  Created by G on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
