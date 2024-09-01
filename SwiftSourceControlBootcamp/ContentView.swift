4f91fc9324ef8b424a0359913f15cac189ded6c6
lostdawg3 <lostdawg3@yahoo.com>
2024-08-31 20:59:08 -0700

    [Clean] Add commit message documentation

--- SwiftSourceControlBootcamp/ContentView.swift
+++ SwiftSourceControlBootcamp/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by G on 8/30/24.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [release] Description of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {
