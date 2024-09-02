d477048236944f95b26884c8079000711b87404a
lostdawg3 <lostdawg3@yahoo.com>
2024-09-01 17:35:39 -0700

    [Feature] Subscribe now added back

--- SwiftSourceControlBootcamp/ContentView.swift
+++ SwiftSourceControlBootcamp/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful!")
             
-            Button("Subscribe") {
+            Button("Subscribe now!") {
                 
             }
             
