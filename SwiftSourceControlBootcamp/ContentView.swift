4779374c409c6cefeaad2f392567f06eceb72476
lostdawg3 <lostdawg3@yahoo.com>
2024-09-01 21:31:34 -0700

    [Feature] Update title name

--- SwiftSourceControlBootcamp/ContentView.swift
+++ SwiftSourceControlBootcamp/ContentView.swift
@@ -48,7 +48,7 @@ struct ContentView: View {
                             Image(systemName: "magnifyingglass")
                                 .font(.largeTitle)
                                 .foregroundStyle(.tint)
-                            Text("Some New Title!")
+                            Text("Swiftful Thinking!!!!")
                             
                             Button("Click me!") {
                                 
