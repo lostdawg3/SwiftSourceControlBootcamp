3b0ebf720a44e84bc8d33248e640747be458d34f
lostdawg3 <lostdawg3@yahoo.com>
2024-09-03 11:16:57 -0700

    Fixed

--- SwiftSourceControlBootcamp/ContentView.swift
+++ SwiftSourceControlBootcamp/ContentView.swift
@@ -52,7 +52,7 @@ struct ContentView: View {
                             Image(systemName: "globe")
                                 .font(.largeTitle)
                                 .foregroundStyle(.tint)
-                            Text("Swiftful Thinking!!!!")
+                            Text("Swiftful Thinking!")
                             
                             Button("Subscribe!") {
                                 
