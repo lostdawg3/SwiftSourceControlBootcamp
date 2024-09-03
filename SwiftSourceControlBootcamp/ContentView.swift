32b07276f1678946dc70d2fdac4d6917708e05ba
lostdawg3 <lostdawg3@yahoo.com>
2024-09-03 15:09:28 -0700

    [Clean] Increment build version to 0.0.1

--- SwiftSourceControlBootcamp/ContentView.swift
+++ SwiftSourceControlBootcamp/ContentView.swift
@@ -23,8 +23,15 @@
  BRANCH RULES
  CodeOwners = go to the source in github, go to add file and create a new file -> .github/ -> CODEOWNERS (Swiftful Thinking - How to Add Branch Rules, CodeOwners, Pull Request (video #11, 12:20)
  PR Templates
+ Protecting Branches
  GitIgnore = create a new file called .gitignore
  Readme = create a new file called README.md (this can be done to projects for project based readme or it can be done for the your whole github readme, depending on where you create the readme file.
+ Releases, Tags, Versioning
+ Versions = 1.0.0 is typically the first major release,
+    When you first start you typically want to have a version number of 0.0.1 where the first #(0) is the major version(big updates), the second #(0) is the minor version(minor updates or features), and the last #(1) is the patch version (fixing a bug)
+    For the build number the old way to increase the date (ex. 20240903),
+        then they would increase the build on every change (ex for 1.0.0 -> build # = 1,2,3,4,5, then reset for 2.0.0),
+        and most recently the build number would increase for any change continuously (1...100+) regardless of version
  
  COMMIT MESSAGES
  
