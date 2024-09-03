bb79791ae522c34a48239b677fac3d27149fcff1
lostdawg3 <lostdawg3@yahoo.com>
2024-09-03 15:57:59 -0700

    [Feature] Add ProfileView

---
+++ SwiftSourceControlBootcamp/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftSourceControlBootcamp
+//
+//  Created by G on 9/3/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Lostdawg3")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
